9bcc1be71bc5821f160302fc9de236209c8deb65
ceb <vivu1991@mail.ru>
2022-09-17 19:58:47 +0300

    Решение задачи №54

---
+++ DZ001/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void PrintMatrix(int [,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] FillMatrix(int line, int column)
+{
+    int [,] array = new int [line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array [i,j] = new Random().Next(1,10);
+        }    
+    }
+    return array;
+}
+
+int [,] SortLines(int [,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1)-1; j++)
+        {
+            for (int l = 0; l < massiv.GetLength(1)-1; l++)
+            {
+                if (massiv[i,l] < massiv[i,l+1])
+                {
+                    int temp = 0;
+                    temp = massiv[i,l];
+                    massiv[i,l] = massiv[i,l+1];
+                    massiv[i,l+1] = temp;
+                }
+            }
+        }
+    }
+    return massiv;
+}
+
+int [,] mas = FillMatrix(3,3);
+Console.WriteLine("Массив до сортировки: ");
+PrintMatrix(mas);
+int [,] mas2 = SortLines(mas);
+Console.WriteLine("Массив после сортировки: ");
+PrintMatrix(mas2);
